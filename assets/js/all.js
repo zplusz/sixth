9a779a9ea0864a7d5b2050f43b5cbd6406c70b41
zplusz <washida0209@yahoo.com.tw>
2020-06-24 21:52:21 +0800

    Update 2020-06-24T13:52:19.602Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,4 +1,12 @@
 "use strict";
 
-$(document).ready(function () {});
+$(document).ready(function () {
+  $('#cart').click(function () {
+    localStorage.setItem("id", "#exampleModal");
+    localStorage.setItem("show", "show");
+  });
+  $(localStorage.getItem("id")).modal(localStorage.getItem("show"));
+  console.log(localStorage.getItem("id"));
+  localStorage.clear();
+});
 //# sourceMappingURL=all.js.map
